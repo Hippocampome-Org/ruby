cd126a1f833ad331c2938454cbd58d1dbfeb09f9
Sean Mackesey <s.mackesey@gmail.com>
2013-12-20 18:03:26 -0500

    change to mysql from mysql2

--- db/db_connection.rb
+++ db/db_connection.rb
@@ -1,1 +1,1 @@
-DB = Sequel.mysql2(DB_NAME, user: DB_USERNAME, password: DB_PASSWORD, encoding: DB_ENCODING)
+DB = Sequel.mysql(DB_NAME, user: DB_USERNAME, password: DB_PASSWORD, encoding: DB_ENCODING)
