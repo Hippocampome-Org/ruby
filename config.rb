ba1e5c48a5928a9b580d071ec7c6651b8d032ae3
Sean Mackesey <s.mackesey@gmail.com>
2013-12-20 14:14:15 -0500

    add sequel dep

--- config.rb
+++ config.rb
@@ -4,7 +4,7 @@ require 'pry'
 
 DB_NAME = "hippocampome"
 DB_USERNAME = "root"
-DB_PASSWORD = "djh-mbpr"
+DB_PASSWORD = "orgslice"
 DB_ENCODING = "utf8"
 
 PORTING_LIBRARY = {
@@ -68,7 +68,7 @@ ERROR_DATA = [
 ]
 
 DATA_DIRECTORY = File.expand_path("../data", "__FILE__")
-EXTERNAL_SOURCE_DATA_DIRECTORY = "/Users/djh/wd/portal/dat/csv"
+EXTERNAL_SOURCE_DATA_DIRECTORY = "/Users/seanmackesey/google_drive/data/hippocampome"
 SOURCE_DATA_DIRECTORY = File.expand_path('source', DATA_DIRECTORY)
 HELPER_DATA_DIRECTORY = File.expand_path('helper', DATA_DIRECTORY)
 ERROR_DATA_DIRECTORY = DATA_DIRECTORY
